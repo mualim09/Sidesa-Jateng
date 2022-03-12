9dd5ba44c5dbc38533eb09760c300364693e1878
zakezone <zakezone69@gmail.com>
2022-03-12 16:02:44 +0700

    init layanan desa menu

--- app/Views/sidesa/layout/pemkab/sidebar_desa.php
+++ app/Views/sidesa/layout/pemkab/sidebar_desa.php
@@ -92,6 +92,78 @@
                         <?php endif; ?>
                     <?php endforeach; ?>
                 </li>
+
+                <!-- Sementara -->
+                <?php
+                $request = \Config\Services::request();
+                $url = $request->uri->getSegment(3);
+                ?>
+                <?php if ($url === "33.29.15.2007") : ?>
+                    <!-- end sementara -->
+                    <li>
+                        <a href="javascript: void(0);" class="has-arrow">
+                            <i data-feather="edit"></i>
+                            <span data-key="t-datakabupaten">Layanan</span>
+                        </a>
+                        <ul class="sub-menu" aria-expanded="false">
+                            <li>
+                                <a href="<?= base_url('pemkab/kependudukandes/' . substr($kodedes, 0, 13)); ?>">
+                                    <span data-key="t-kependudukankab">Informasi</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= base_url('pemkab/kesejahteraandes/' . substr($kodedes, 0, 13)); ?>">
+                                    <span data-key="t-kesejahteraan-sosialkab">Pengajuan KTP</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= base_url('pemkab/keuangandes/' . substr($kodedes, 0, 13)); ?>">
+                                    <span data-key="t-keuangankab">Pengajuan Akte Kelahiran</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= base_url('pemkab/disabilitasdes/' . substr($kodedes, 0, 13)); ?>">
+                                    <span data-key="t-disabilitaskab">Disabilitas</span>
+                                </a>
+                            </li>
+                            <!-- <li>
+                            <a href="<?= base_url('pemkab/danadesades/' . substr($kodedes, 0, 13)); ?>">
+                                <span data-key="t-danadesakab">Dana Desa</span>
+                            </a>
+                        </li> -->
+                            <li>
+                                <a href="<?= base_url('pemkab/rtlhdes/' . substr($kodedes, 0, 13)); ?>">
+                                    <span data-key="t-rtlhkab">RTLH</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= base_url('pemkab/idmdes/' . substr($kodedes, 0, 13)); ?>">
+                                    <span data-key="t-idmdes">IDM</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= base_url('pemkab/bumdesdes/' . substr($kodedes, 0, 13)); ?>">
+                                    <span data-key="t-bumdeskab">Bumdes</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= base_url('pemkab/kesehatandes/' . substr($kodedes, 0, 13)); ?>">
+                                    <span data-key="t-kesehatankab">Kesehatan</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= base_url('pemkab/pertaniandes/' . substr($kodedes, 0, 13)); ?>">
+                                    <span data-key="t-pertaniankab">Pertanian</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= base_url('pemkab/sosialbudayades/' . substr($kodedes, 0, 13)); ?>">
+                                    <span data-key="t-sosialbudayakab">Sosial Budaya</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                <?php endif; ?>
             </ul>
         </div>
         <!-- Sidebar -->
